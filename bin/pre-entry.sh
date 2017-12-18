166f6c4c3dbc0eba0ff881dec4d365763dd6cd3b
huggla <henrik.uggla@kristianstad.se>
2017-12-18 13:45:37 +0100

    Update pre-entry.sh

--- bin/pre-entry.sh
+++ bin/pre-entry.sh
@@ -1,6 +1,13 @@
 #!/bin/sh -e
 
 mkdir -p /overlay/hostdir/`hostname`-upperdir \
-         /overlay/hostdir/`hostname`-workdir
+         /overlay/hostdir/`hostname`-workdir \
+         /tmp/pgdata
+         
+chown postgres:postgres /tmp/pgdata
 
-mount -t overlay overlay -o lowerdir=/overlay/lowerdir,upperdir=/overlay/hostdir/`hostname`-upperdir,workdir=/overlay/hostdir/`hostname`-workdir /var/lib/postgresql/data/pgdata
+rm -rf /var/lib/postgresql/data/pgdata
+
+ln -s /tmp/pgdata /var/lib/postgresql/data/
+
+mount -t overlay overlay -o lowerdir=/overlay/lowerdir,upperdir=/overlay/hostdir/`hostname`-upperdir,workdir=/overlay/hostdir/`hostname`-workdir /tmp/pgdata
