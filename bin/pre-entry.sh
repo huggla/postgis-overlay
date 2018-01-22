9f40c849a6b9599e6b84c28ea56d1781b929a009
huggla <henrik.uggla@kristianstad.se>
2018-01-22 11:18:15 +0100

    Update pre-entry.sh

--- bin/pre-entry.sh
+++ bin/pre-entry.sh
@@ -1,6 +1,20 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
+#Define cleanup procedure
+cleanup() {
+    echo "Container stopped, performing cleanup..."
+    rm -rf /overlay/hostdir/`hostname`-upperdir
+    rm -rf /overlay/hostdir/`hostname`-workdir
+}
+
+#Trap SIGTERM
+trap 'cleanup' SIGTERM
+
+#Execute commands
 mkdir -p /overlay/hostdir/`hostname`-upperdir \
          /overlay/hostdir/`hostname`-workdir
+mount -t overlay overlay -o lowerdir=/overlay/lowerdir,upperdir=/overlay/hostdir/`hostname`-upperdir,workdir=/overlay/hostdir/`hostname`-workdir /tmp/pgdata \
+&& docker-entrypoint.sh postgres &
 
-mount -t overlay overlay -o lowerdir=/overlay/lowerdir,upperdir=/overlay/hostdir/`hostname`-upperdir,workdir=/overlay/hostdir/`hostname`-workdir /tmp/pgdata
+#Wait
+wait $!
